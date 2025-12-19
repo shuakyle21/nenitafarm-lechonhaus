8d9473cdece10ccc904bfb902cb4f9f4c1da740f
shuakyle21 <jkppudadera@ndmu.edu.ph>
2025-12-19 18:27:06 +0800

    feat: Create InventoryPage for routing

---
+++ src/pages/InventoryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InventoryModule from '@/components/InventoryModule';
+
+const InventoryPage: React.FC = () => {
+  return (
+    <div className="p-4 md:p-8 max-w-7xl mx-auto">
+      <InventoryModule />
+    </div>
+  );
+};
+
+export default InventoryPage;
