b9a139ec3abe2e0c4cb423ed8417859c0c156059
shuakyle21 <nenitarmo@gmail.com>
2026-01-29 12:14:07 +0800

    chore: move App Insights connection string to environment variables

--- src/services/appInsights.ts
+++ src/services/appInsights.ts
@@ -1,11 +1,11 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 
-const connectionString = 'InstrumentationKey=8a01c1c8-34e7-42a9-88a1-0fa052b5c22c;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/;ApplicationId=6f012df7-1503-4e76-9d23-7ef5b9366e3f';
+const connectionString = import.meta.env.VITE_APP_INSIGHTS_CONNECTION_STRING;
 
 const appInsights = new ApplicationInsights({
   config: {
     connectionString: connectionString,
-    enableAutoRouteTracking: true, // Also enabling auto route tracking for better insights
+    enableAutoRouteTracking: true,
   },
 });
 
